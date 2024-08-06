00c4d40657bf08ff6ecdc8eb1023ec73742b0f23
amaechijude <ugwuamaechi5@gmail.com>
2024-08-06 17:14:54 +0100

    stack

--- python/stack/linkedlistStack.py
+++ python/stack/linkedlistStack.py
@@ -2,43 +2,63 @@
 
 # create a Node class to hold the linked list
 class Node:
-    def __init__(self, data) -> None:
+    def __init__(self, data=None) -> None:
         self.data = data
         self.next = None
 
 
 # Create the stack class and methods
 class Stack:
-    def __init__(self) -> None:
+    def __init__(self):
         self.headNode = None
         self.size = 0
-        
-    def push(self, element=None):
-        newNode = Node(element)
-        currentNode = self.headNode
-        
-        if self.headNode == None:
-            return newNode
-        
-        while currentNode and currentNode.next == None:
-            self.headNode.next = newNode
-            currentNode = currentNode.next
 
-        return self.headNode
-    
+    def show(self):
+        if self.headNode is None:
+            print("Empty")
+        else:
+            currentNode = self.headNode
+            while currentNode:
+                print(f"{currentNode.data}", end=" -> ")
+                currentNode = currentNode.next
+            print('end')
+        
+    def push(self, element):
+        newNode = Node(element)
+        newNode.next = self.headNode
+        self.headNode = newNode
+
     def pop(self):
+        temp = self.headNode
+        self.headNode = self.headNode.next
+        return temp.data
+
+    def peek(self):
+        return self.headNode
+
+    def isEmpty(self) -> bool:
+        print(self.headNode is None)
+
+    def listSize(self) -> int:
+        if self.headNode is None:
+            return self.size
+        
         currentNode = self.headNode
-        if currentNode == None:
-            return "Empty stack"
-        while currentNode.next and currentNode.next.next != None:
+        while currentNode:
+            self.size += 1
             currentNode = currentNode.next
-        temp = currentNode.next.data
-        currentNode = None
-        return temp
+        return f"Total nodes is : {self.size}"
+
+
 
 stack = Stack()
 stack.push("chai")
-
-stack.push("chai")
-
-print(stack.pop())
+stack.push("abia")
+stack.push("okay")
+stack.show()
+stack.pop()
+stack.show()
+stack.push("haha")
+stack.push("egwu")
+stack.show()
+stack.listSize()
